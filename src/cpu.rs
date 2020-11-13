177ff2eb127876d000f24aa377803e132fda6a01
Om Prakaash <omsuseela@gmail.com>
2020-11-13 07:49:54 +0530

    Added add, sub, ld opcodes, updated alu-sub op to return result

--- src/cpu.rs
+++ src/cpu.rs
@@ -120,6 +120,110 @@ impl<'a> CPU<'a>{
             0x3E => { self.reg.a = self.read_next_byte(); 2}
             0x3F => { 1 } // TODO: CCF
 
+            0x40 => { 1  } // just loads b to b ( does nothing ) - ld b, b
+            0x41 => { self.reg.b = self.reg.c; 1}
+            0x42 => { self.reg.b = self.reg.d; 1 }
+            0x43 => { self.reg.b = self.reg.e; 1}
+            0x44 => { self.reg.b = self.reg.h; 1}
+            0x45 => { self.reg.b = self.reg.l; 1}
+            0x46 => { self.reg.b = self.mmu.read_byte(self.reg.get_hl()); 2 }
+            0x47 => { self.reg.b = self.reg.a; 1}
+            0x48 => { self.reg.c = self.reg.b; 1}
+            0x49 => { 1 } // Redundant - ld c, c
+            0x4A => { self.reg.c = self.reg.d; 1}
+            0x4B => { self.reg.c = self.reg.e ; 1 }
+            0x4C => { self.reg.c = self.reg.h; 1}
+            0x4D => { self.reg.c = self.reg.l; 1}
+            0x4E => { self.reg.c = self.mmu.read_byte(self.reg.get_hl()); 2}
+            0x4F => { self.reg.c = self.reg.a; 1}
+
+            0x50 => { self.reg.d = self.reg.b; 1}
+            0x51 => { self.reg.d = self.reg.c; 1}
+            0x52 => { 1 } // ld d, d
+            0x53 => { self.reg.d = self.reg.e; 1}
+            0x54 => { self.reg.d = self.reg.h; 1}
+            0x55 => { self.reg.d = self.reg.l; 1}
+            0x56 => { self.reg.d = self.mmu.read_byte(self.reg.get_hl()); 2}
+            0x57 => { self.reg.d = self.reg.a; 1}
+            0x58 => { self.reg.e = self.reg.b; 1}
+            0x59 => { self.reg.e = self.reg.c; 1}
+            0x5A => { self.reg.e = self.reg.d; 1}
+            0x5B => { 1 } // ld e, e
+            0x5C => { self.reg.e = self.reg.h; 1}
+            0x5D => { self.reg.e = self.reg.l; 1}
+            0x5E => { self.reg.e = self.mmu.read_byte(self.reg.get_hl()); 2}
+            0x5F => { self.reg.e = self.reg.a; 1}
+
+            0x60 => { self.reg.h = self.reg.b; 1}
+            0x61 => { self.reg.h = self.reg.c; 1}
+            0x62 => { self.reg.h = self.reg.d; 1 } // ld d, d
+            0x63 => { self.reg.h = self.reg.e; 1}
+            0x64 => { 1 } // LD h, h
+            0x65 => { self.reg.h = self.reg.l; 1}
+            0x66 => { self.reg.h = self.mmu.read_byte(self.reg.get_hl()); 2}
+            0x67 => { self.reg.h = self.reg.a; 1}
+            0x68 => { self.reg.l = self.reg.b; 1}
+            0x69 => { self.reg.l = self.reg.c; 1}
+            0x6A => { self.reg.l = self.reg.d; 1}
+            0x6B => { self.reg.l = self.reg.e; 1 } // ld e, e
+            0x6C => { self.reg.l = self.reg.h; 1}
+            0x6D => { 1 } // ld l, l
+            0x6E => { self.reg.l = self.mmu.read_byte(self.reg.get_hl()); 2}
+            0x6F => { self.reg.l = self.reg.a; 1}
+
+            0x70 => { self.mmu.write_byte(self.reg.get_hl(), self.reg.b); 2 }
+            0x71 => { self.mmu.write_byte(self.reg.get_hl(), self.reg.c); 2 }
+            0x72 => { self.mmu.write_byte(self.reg.get_hl(), self.reg.d); 2 }
+            0x73 => { self.mmu.write_byte(self.reg.get_hl(), self.reg.e); 2 }
+            0x74 => { self.mmu.write_byte(self.reg.get_hl(), self.reg.h); 2 }
+            0x75 => { self.mmu.write_byte(self.reg.get_hl(), self.reg.l); 2 }
+            0x76 => { 1 } // TODO: HALT
+            0x77 => { self.mmu.write_byte(self.reg.get_hl(), self.reg.a); 2 }
+            0x78 => { self.reg.a = self.reg.b; 1 }
+            0x79 => { self.reg.a = self.reg.c; 1 }
+            0x7A => { self.reg.a = self.reg.d; 1 }
+            0x7B => { self.reg.a = self.reg.e; 1 } 
+            0x7C => { self.reg.a = self.reg.h; 1 }
+            0x7D => { self.reg.a = self.reg.l; 1 } 
+            0x7E => { self.reg.a = self.mmu.read_byte(self.reg.get_hl()); 2 }
+            0x7F => { 1 } // ld a, a
+
+            0x80 => { self.reg.a = self.alu_add(self.reg.b); 1 }
+            0x81 => { self.reg.a = self.alu_add(self.reg.c); 1 }
+            0x82 => { self.reg.a = self.alu_add(self.reg.d); 1 }
+            0x83 => { self.reg.a = self.alu_add(self.reg.e); 1 }
+            0x84 => { self.reg.a = self.alu_add(self.reg.h); 1 }
+            0x85 => { self.reg.a = self.alu_add(self.reg.l); 1 }
+            0x86 => { self.reg.a = self.alu_add(self.mmu.read_byte(self.reg.get_hl())); 2 }
+            0x87 => { self.reg.a = self.alu_add(self.reg.a); 1 }
+            0x88 => { self.reg.a = self.alu_adc(self.reg.b); 1 }
+            0x89 => { self.reg.a = self.alu_adc(self.reg.c); 1 }
+            0x8A => { self.reg.a = self.alu_adc(self.reg.d); 1 }
+            0x8B => { self.reg.a = self.alu_adc(self.reg.e); 1 }
+            0x8C => { self.reg.a = self.alu_adc(self.reg.h); 1 }
+            0x8D => { self.reg.a = self.alu_adc(self.reg.l); 1 }
+            0x8E => { self.reg.a = self.alu_adc(self.mmu.read_byte(self.reg.get_hl())); 2 }
+            0x8F => { self.reg.a = self.alu_adc(self.reg.a); 1 }
+
+            0x90 => { self.reg.a = self.alu_sub(self.reg.b); 1}
+            0x91 => { self.reg.a = self.alu_sub(self.reg.c); 1}
+            0x92 => { self.reg.a = self.alu_sub(self.reg.d); 1}
+            0x93 => { self.reg.a = self.alu_sub(self.reg.e); 1}
+            0x94 => { self.reg.a = self.alu_sub(self.reg.h); 1}
+            0x95 => { self.reg.a = self.alu_sub(self.reg.l); 1}
+            0x96 => { self.reg.a = self.alu_sub(self.mmu.read_byte(self.reg.get_hl())); 2}
+            0x97 => { self.reg.a = self.alu_sub(self.reg.a); 1}
+            0x98 => { self.reg.a = self.alu_sbc(self.reg.b); 1}
+            0x99 => { self.reg.a = self.alu_sbc(self.reg.c); 1}
+            0x9A => { self.reg.a = self.alu_sbc(self.reg.d); 1}
+            0x9B => { self.reg.a = self.alu_sbc(self.reg.e); 1}
+            0x9C => { self.reg.a = self.alu_sbc(self.reg.h); 1}
+            0x9D => { self.reg.a = self.alu_sbc(self.reg.l); 1}
+            0x9E => { self.reg.a = self.alu_sbc(self.mmu.read_byte(self.reg.get_hl())); 2}
+            0x9F => { self.reg.a = self.alu_sbc(self.reg.a); 1}
+
+
+
             0xAF => {println!("{:#x} XOR with A", instr); self.alu_xor(self.reg.a); 1}
             0xFE => {println!("Compare"); let n = self.read_next_byte(); self.alu_cmp(n); 2 }
             _ => {panic!("Unrecognized opcode") ; }
@@ -148,13 +252,21 @@ impl<'a> CPU<'a>{
     }
 
     // Might want to change to account for sbc or just create a sbc function
-    fn alu_sub(&mut self, n: u8){
+    fn alu_sub(&mut self, n: u8) -> u8{
         self.flags.H = (self.reg.a & 0xf) < (n & 0xf) ;
         self.flags.C = self.reg.a < n;
-        self.reg.a = self.reg.a.wrapping_sub(n);
-        // Setting Flgs
+        let new_val = self.reg.a.wrapping_sub(n);
+        // Setting Flags
         self.flags.Z = self.reg.a == 0;
         self.flags.N = true;
+        new_val
+    }
+
+    fn alu_sbc(&mut self, n: u8) -> u8{
+        match self.flags.C{
+            true => self.alu_sub(n + 1),
+            false => self.alu_sub(n)
+        }
     }
 
     fn alu_cmp(&mut self, val: u8 ) -> u8{
@@ -167,6 +279,13 @@ impl<'a> CPU<'a>{
         self.flags.C = false;
         self.flags.H = false;
         self.flags.N = false;
+    }
+
+    fn alu_adc(&mut self, n: u8) -> u8 {
+        match self.flags.C{
+            true => self.alu_add(n + 1),
+            false => self.alu_add(n)
+        }
     }
 
     fn alu_add(&mut self, n:u8) -> u8{
@@ -205,4 +324,6 @@ impl<'a> CPU<'a>{
         self.flags.C = (cur_val as u32) + (new_val as u32) > 0xFFFF;
         new_val
     }
+
+    fn alu_and()
 }
